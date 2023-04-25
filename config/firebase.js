c8f09e08a989f88328b5dfa25e8f1e22998586d4
ayan.shukla <ayan.shukla@flipkart.com>
2023-04-26 02:41:56 +0530

    feat: added firebase config

---
+++ config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import constant from 'expo-constants';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: constant.manifest?.extra?.apiKey,
+  authDomain: constant.manifest?.extra?.authDomain,
+  projectId: constant.manifest?.extra?.projectId,
+  storageBucket: constant.manifest?.extra?.storageBucket,
+  messagingSenderId: constant.manifest?.extra?.messagingSenderId,
+  appId: constant.manifest?.extra?.appId
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
